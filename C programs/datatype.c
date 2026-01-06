97963e80a2d703e47b65d9a9c41018e57e771d0b
JanhaviMupde <mupdejanhavi@gmail.com>
2026-01-06 14:02:14 +0530

    Add files via upload

---
+++ C programs/datatype.c
@@ -0,0 +1,18 @@
+//28 august 2025
+#include<stdio.h>
+
+int main()
+{
+    char cValue = 'S';
+    int iValue = 11;
+    float fValue = 90.78f;
+    double dValue = 98.45678;
+
+    printf("%c\n",cValue);
+    printf("%d\n",iValue);
+    printf("%f\n",fValue);
+    printf("%lf\n",dValue);
+
+    return 0;
+}
+//done
